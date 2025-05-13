e88386c8013cb343774568ae48418c649bf9ea48
Alan Sebastian <aseb0007@student.monash.edu>
2025-05-13 20:20:02 +1000

    Implemented the EatBeetleAction through the creation of a new action

--- src/game/actors/npcs/GoldenBeetle.java
+++ src/game/actors/npcs/GoldenBeetle.java
@@ -3,11 +3,14 @@ package game.actors.npcs;
 import java.util.HashMap;
 import java.util.Map;
 import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.positions.Exit;
 import edu.monash.fit2099.engine.positions.GameMap;
 import edu.monash.fit2099.engine.actors.Behaviour;
 import edu.monash.fit2099.engine.actions.Action;
 import edu.monash.fit2099.engine.actions.ActionList;
 import edu.monash.fit2099.engine.displays.Display;
+import edu.monash.fit2099.engine.positions.Location;
+import game.actions.EatBeetleAction;
 import game.capabilities.Status;
 import game.behaviours.FollowBehaviour;
 import game.behaviours.WanderBehaviour;
@@ -31,11 +34,41 @@ public class GoldenBeetle extends Actor{
         turnsSinceLastEgg++;
 
         if (turnsSinceLastEgg >=5) {
-            map.locationOf(this).addItem(new GoldenEgg);
+            map.locationOf(this).addItem(new GoldenEgg());
             turnsSinceLastEgg = 0;
-            display.println(this + " has layed a Golden Egg");
+            display.println(this + " has laid a Golden Egg");
             return null;
         }
+
+        if (followTarget == null) {
+            Location here = map.locationOf(this);
+            for (Exit exit : here.getExits()) {
+                Actor potentialTarget = exit.getDestination().getActor();
+                if (potentialTarget != null && potentialTarget.hasCapability(Status.FOLLOWABLE)) {
+                    followTarget = potentialTarget;
+                    behaviours.put(1, new FollowBehaviour(followTarget));
+                    break;
+                }
+            }
+        }
+
+        for (Behaviour behaviour : behaviours.values()) {
+            Action action = behaviour.getAction(this, map);
+            if (action != null) {
+                return action;
+            }
+        }
+
+        return null;
+    }
+
+    @Override
+    public ActionList allowableActions(Actor otherActor, String direction, GameMap map) {
+        ActionList actions = new ActionList();
+        if (otherActor.hasCapability(Status.PLAYER)) {
+            actions.add(new EatBeetleAction(this));
+        }
+        return actions;
     }
 
 
