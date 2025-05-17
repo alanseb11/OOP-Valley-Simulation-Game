1e5965c18dd95b5a4e1b070be7f142e17832ba34
owil0005 <owil0005@student.monash.edu>
2025-05-17 21:56:58 +1000

    Added javadoc comments to purchaseaction

--- src/game/actions/PurchaseAction.java
+++ src/game/actions/PurchaseAction.java
@@ -6,15 +6,18 @@ import edu.monash.fit2099.engine.positions.GameMap;
 import game.interfaces.Merchant;
 import game.purchaseeffects.MerchantOffer;
 
+/**
+ * Represents an action to purchase an item from a Merchant.
+ */
 public class PurchaseAction extends Action {
     private MerchantOffer offer;
     private Merchant merchant;
     private Actor buyer;
 
     /**
-     * Constructor.
+     * Constructor for the PurchaseAction.
      *
-     * @param offer  The offer to be purchased
+     * @param offer The offer to be purchased
      * @param merchant The Merchant selling the offer
      * @param buyer The Actor that is buying the offer
      */
@@ -24,11 +27,24 @@ public class PurchaseAction extends Action {
         this.buyer = buyer;
     }
 
+    /**
+     * Executes the purchase action, completing the purchase of the offer.
+     *
+     * @param actor The actor performing the action
+     * @param map The game map where the action is performed
+     * @return a string describing the result of the purchase
+     */
     @Override
     public String execute(Actor actor, GameMap map) {
         return offer.completePurchase(buyer, map);
     }
 
+    /**
+     * Returns a description of the purchase action for the menu.
+     *
+     * @param actor The actor performing the action
+     * @return a string description of the purchase action
+     */
     @Override
     public String menuDescription(Actor actor) {
         return actor + " purchases a " + offer.getItem() + " from " + merchant;
