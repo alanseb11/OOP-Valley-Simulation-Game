4b65911d00ef14d064ad90e7b1ff18212187cb0c
Alan Sebastian <aseb0007@student.monash.edu>
2025-06-09 16:38:13 +1000

    Added JavaDoc to all classes

--- src/game/actors/npcs/boss/BedOfChaos.java
+++ src/game/actors/npcs/boss/BedOfChaos.java
@@ -16,16 +16,27 @@ import game.weapons.BedOfChaosIntrinsicWeapon;
 import game.behaviours.AttackBehaviour;
 import game.actions.AttackAction;
 
-
 import java.util.*;
 import java.util.function.Predicate;
 
+/**
+ * Represents the Bed of Chaos boss NPC in the game.
+ *
+ * <p>
+ * This boss is stationary, grows branches and leaves, and can attack the player.
+ * Each branch and leaf contributes to its total attack damage.
+ * It implements the {@link Growable} interface to manage its growth behavior.
+ * </p>
+ */
 public class BedOfChaos extends Actor implements Growable {
     private final int baseDamage = 25;
     private List<BossPart> parts = new ArrayList<>();
     private final Random random = new Random();
     protected Map<Integer, Behaviour> behaviours = new HashMap<>();
 
+    /**
+     * Constructor to create a new Bed of Chaos instance with base health and behaviors.
+     */
     public BedOfChaos() {
         super("Bed of Chaos", 'T', 1000);
 
@@ -37,10 +48,21 @@ public class BedOfChaos extends Actor implements Growable {
 
     }
 
+    /**
+     * Gets the base damage of the Bed of Chaos.
+     *
+     * @return base damage value
+     */
     public int getBaseDamage() {
         return baseDamage;
     }
 
+    /**
+     * Calculates the total damage of the Bed of Chaos by adding
+     * the damage of all grown parts (branches and leaves).
+     *
+     * @return total attack damage
+     */
     public int getTotalAttackDamage() {
         int totalDamage = 0;
         for (BossPart part : parts) {
@@ -49,6 +71,12 @@ public class BedOfChaos extends Actor implements Growable {
         return totalDamage;
     }
 
+    /**
+     * Handles the growth behavior of the Bed of Chaos.
+     * Randomly grows a new branch (which may recursively grow sub-parts) or a leaf.
+     *
+     * @return description of the growth event
+     */
     @Override
     public String grow() {
         if (random.nextBoolean()) {
@@ -63,6 +91,13 @@ public class BedOfChaos extends Actor implements Growable {
         }
     }
 
+    /**
+     * Determines whether the Bed of Chaos can grow.
+     * Growth is not allowed if a player is adjacent.
+     *
+     * @param map the game map containing this boss
+     * @return true if it can grow, false otherwise
+     */
     @Override
     public boolean canGrow(GameMap map) {
         // Get the map containing BedOfChaos
@@ -75,11 +110,26 @@ public class BedOfChaos extends Actor implements Growable {
         return true;
     }
 
+    /**
+     * Returns a string representation of the Bed of Chaos, including
+     * its current health, number of parts, and total damage.
+     *
+     * @return string representation
+     */
     @Override
     public String toString() {
         return String.format("Bed of Chaos [HP: %d, Parts: %d, Damage: %d]", this.getAttribute(BaseActorAttributes.HEALTH) , parts.size(), getBaseDamage() + getTotalAttackDamage());
     }
 
+    /**
+     * Returns the list of actions other actors can perform on the Bed of Chaos.
+     * Allows the player (HOSTILE_TO_ENEMY) to attack it.
+     *
+     * @param otherActor the actor interacting with the boss
+     * @param direction  the direction of the boss relative to the other actor
+     * @param map        the current game map
+     * @return an ActionList of possible actions
+     */
     @Override
     public ActionList allowableActions(Actor otherActor, String direction, GameMap map) {
         ActionList actions = new ActionList();
@@ -89,6 +139,16 @@ public class BedOfChaos extends Actor implements Growable {
         return actions;
     }
 
+    /**
+     * Determines and executes the action the Bed of Chaos performs each turn.
+     * Prioritizes behaviors based on their defined priority.
+     *
+     * @param actions    the list of possible actions
+     * @param lastAction the action taken in the previous turn
+     * @param map        the game map
+     * @param display    the display object for rendering messages
+     * @return the action to be executed
+     */
     @Override
     public Action playTurn(ActionList actions, Action lastAction, GameMap map, Display display) {
         for (edu.monash.fit2099.engine.actors.Behaviour behaviour : behaviours.values()) {
--- src/game/weapons/BedOfChaosIntrinsicWeapon.java
+++ src/game/weapons/BedOfChaosIntrinsicWeapon.java
@@ -7,14 +7,50 @@ import game.actors.npcs.boss.BedOfChaos;
 
 import java.util.Random;
 
+/**
+ * Represents the intrinsic weapon used by the {@link BedOfChaos} boss.
+ *
+ * <p>
+ * This weapon combines the base damage of the Bed of Chaos with the accumulated
+ * damage from its grown parts (e.g., branches and leaves).
+ * It has a 75% chance to hit.
+ * </p>
+ *
+ * <p>
+ * If the attack misses, a message is returned indicating the miss.
+ * If the attack hits, the target takes the calculated damage.
+ * </p>
+ *
+ * @author YourName
+ */
+
 public class BedOfChaosIntrinsicWeapon extends IntrinsicWeapon {
     private final BedOfChaos boss;
 
+    /**
+     * Constructor for the BedOfChaosIntrinsicWeapon.
+     *
+     * @param boss the instance of Bed of Chaos using this weapon
+     */
     public BedOfChaosIntrinsicWeapon(BedOfChaos boss) {
         super(25,"lashes out at", 75);
         this.boss = boss;
     }
 
+    /**
+     * Performs an attack using this intrinsic weapon.
+     *
+     * <p>
+     * Calculates the total damage by summing the base damage and the damage from all
+     * boss parts (branches and leaves).
+     * If the attack misses based on hit rate, a miss message is returned.
+     * </p>
+     *
+     * @param attacker the actor performing the attack (Bed of Chaos)
+     * @param target   the actor being attacked
+     * @param map      the map where the attack takes place
+     * @return a description of the attack result
+     */
     @Override
     public String attack(Actor attacker, Actor target, GameMap map) {
         int totalDamage = boss.getBaseDamage() + boss.getTotalAttackDamage();
