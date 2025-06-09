f757435ff139956be1f6c3486b23acd67aed523b
Alan Sebastian <aseb0007@student.monash.edu>
2025-06-09 16:19:00 +1000

    Modified BedOfChaos so that it handles inheritance better

--- src/game/actors/npcs/boss/BedOfChaos.java
+++ src/game/actors/npcs/boss/BedOfChaos.java
@@ -1,29 +1,34 @@
 package game.actors.npcs.boss;
 
+import edu.monash.fit2099.engine.actions.DoNothingAction;
 import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.actions.Action;
+import edu.monash.fit2099.engine.actions.ActionList;
+import edu.monash.fit2099.engine.actors.Behaviour;
 import edu.monash.fit2099.engine.actors.attributes.BaseActorAttributes;
+import edu.monash.fit2099.engine.displays.Display;
 import edu.monash.fit2099.engine.positions.Exit;
 import edu.monash.fit2099.engine.positions.GameMap;
-import game.actors.npcs.types.AttackableNPC;
 import game.behaviours.GrowBehaviour;
 import game.capabilities.Status;
 import game.interfaces.Growable;
 import game.weapons.BedOfChaosIntrinsicWeapon;
 import game.behaviours.AttackBehaviour;
+import game.actions.AttackAction;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
+
+import java.util.*;
 import java.util.function.Predicate;
 
-public class BedOfChaos extends AttackableNPC implements Growable {
+public class BedOfChaos extends Actor implements Growable {
     private final int baseDamage = 25;
     private List<BossPart> parts = new ArrayList<>();
     private final Random random = new Random();
+    protected Map<Integer, Behaviour> behaviours = new HashMap<>();
 
     public BedOfChaos() {
         super("Bed of Chaos", 'T', 1000);
-        behaviours.clear();
+
         setIntrinsicWeapon(new BedOfChaosIntrinsicWeapon(this));
 
         Predicate<Actor> isPlayer = actor -> actor.hasCapability(Status.HOSTILE_TO_ENEMY);
@@ -75,5 +80,23 @@ public class BedOfChaos extends AttackableNPC implements Growable {
         return String.format("Bed of Chaos [HP: %d, Parts: %d, Damage: %d]", this.getAttribute(BaseActorAttributes.HEALTH) , parts.size(), getBaseDamage() + getTotalAttackDamage());
     }
 
+    @Override
+    public ActionList allowableActions(Actor otherActor, String direction, GameMap map) {
+        ActionList actions = new ActionList();
+        if (otherActor.hasCapability(Status.HOSTILE_TO_ENEMY)) {
+            actions.add(new AttackAction(this, direction));
+        }
+        return actions;
+    }
 
+    @Override
+    public Action playTurn(ActionList actions, Action lastAction, GameMap map, Display display) {
+        for (edu.monash.fit2099.engine.actors.Behaviour behaviour : behaviours.values()) {
+            Action action = behaviour.getAction(this, map);
+            if (action != null) {
+                return action;
+            }
+        }
+        return new DoNothingAction();
+    }
 }
